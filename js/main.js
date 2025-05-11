31de3b9bc4458505441f7c99cbfa0befc10427a5
Mariana Mendoza Gutierrez <160821469+Marr1731@users.noreply.github.com>
2025-05-11 00:11:26 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const tableBody = document.getElementById('deviceTableBody');
 const firstStatus = document.getElementById('firstStatus');
-const API_URL = 'http://35.170.187.126/api/devices'; // URL de la API
+const API_URL = 'http://54.89.224.38/api/devices'; // URL de la API
 
 async function fetchData() {
     try {
